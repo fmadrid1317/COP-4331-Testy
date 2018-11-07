ed641e1fc3d23189dfb17141ae05b62513ff22c3
Luis <fmadrid1317@knights.ucf.edu>
2018-11-07 17:25:27 -0500

    Implemented Sign Out button on the Main Menu Activity and changed name of the app to just Testy

--- app/src/main/java/com/example/ferna/mytesty/MainMenuActivity.java
+++ app/src/main/java/com/example/ferna/mytesty/MainMenuActivity.java
@@ -5,12 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.gson.Gson;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     public Button newTestButton;
+    public Button signoutButton;
+    private FirebaseAuth mAuth;
+
+
 
     private void init()
     {
@@ -21,6 +27,20 @@ public class MainMenuActivity extends AppCompatActivity {
                 Intent openTestTypes;
                 openTestTypes = new Intent(MainMenuActivity.this, TestTypes.class);
                 startActivity(openTestTypes);
+            }
+        });
+
+        signoutButton = (Button)findViewById(R.id.sign_out_button);
+        signoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+                Toast.makeText(MainMenuActivity.this, "Sign Out Successful",
+                        Toast.LENGTH_SHORT).show();
+                Intent backToLogin;
+                backToLogin = new Intent(MainMenuActivity.this, LoginActivity.class);
+                startActivity(backToLogin);
             }
         });
     }
