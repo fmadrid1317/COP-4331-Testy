ae9e90703eb5d87426994189a22fab605e7aab43
GalacticVoid <brandon.cook.j@gmail.com>
2018-11-09 17:43:51 -0500

    Trying to push quiz object to firebase

--- app/src/main/java/com/example/ferna/mytesty/TestTypes.java
+++ app/src/main/java/com/example/ferna/mytesty/TestTypes.java
@@ -1,11 +1,18 @@
 package com.example.ferna.mytesty;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
 public class TestTypes extends AppCompatActivity {
 
@@ -66,6 +73,7 @@ public class TestTypes extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
+                mDatabase.child("Quizzes").child("QuizName").setValue(newQuiz);
                 finish();
             }
         });
@@ -77,5 +85,11 @@ public class TestTypes extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_types);
         init();
+    }
+
+    private DatabaseReference mDatabase;
+
+    {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
     }
 }
