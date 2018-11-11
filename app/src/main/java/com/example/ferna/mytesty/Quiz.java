f4a29cca5ab59af74763dc4be4274260904a431b
GalacticVoid <brandon.cook.j@gmail.com>
2018-11-11 18:01:07 -0500

    quiz updates

--- app/src/main/java/com/example/ferna/mytesty/Quiz.java
+++ app/src/main/java/com/example/ferna/mytesty/Quiz.java
@@ -1,8 +1,5 @@
 package com.example.ferna.mytesty;
 
-import android.util.Log;
-import android.widget.EditText;
-
 import java.io.Serializable;
 
 
@@ -11,10 +8,14 @@ import java.io.Serializable;
 public class Quiz implements Serializable
 {
     Question head;
+    Question current;
+    int questionCount;
 
     Quiz()
     {
         Question head = null;
+        Question current = null;
+        questionCount = 0;
     }
 
     void addQuest(int questType, String qQuestion, String corAns, String wroAns1, String wroAns2, String wroAns3, Boolean tF)
@@ -22,34 +23,52 @@ public class Quiz implements Serializable
         Question newQuest = new Question(questType, qQuestion, corAns, wroAns1, wroAns2, wroAns3, tF);
         newQuest.next = head;
         newQuest.previous = null;
-        if(newQuest.next == null)
-            Log.d("aaa", "AAAAAAAAAA");
         if (head != null)
             head.previous = newQuest;
 
         head = newQuest;
     }
 
+    void nextQuestion()
+    {
+        if(current == null)
+            current = head;
+        else if(current.next == null)
+            ;
+        else
+            current = current.next;
+    }
+
+    void previousQuestion()
+    {
+        if(current == null)
+            current = head;
+        else if(current.previous == null)
+            ;
+        else
+            current = current.previous;
+    }
+
     class Question implements Serializable
     {
-        int quesType;
-        String quizQuestion;
-        String corrAns;
-        String wrongAns1;
-        String wrongAns2;
-        String wrongAns3;
-        Boolean truFal;
+        private int quesType;
+        private String quizQuestion;
+        private String corrAns;
+        private String wrongAns1;
+        private String wrongAns2;
+        private String wrongAns3;
+        private Boolean truFal;
         Question previous, next;
 
         Question(int questType, String qQuestion, String corAns, String wroAns1, String wroAns2, String wroAns3, Boolean tF)
         {
-            quesType = questType;
-            quizQuestion = qQuestion;
-            corrAns = corAns;
-            wrongAns1 = wroAns1;
-            wrongAns2 = wroAns2;
-            wrongAns3 = wroAns3;
-            truFal = tF;
+            setQuesType(questType);
+            setQuizQuestion(qQuestion);
+            setCorrAns(corAns);
+            setWrongAns1(wroAns1);
+            setWrongAns2(wroAns2);
+            setWrongAns3(wroAns3);
+            setTruFal(tF);
             previous = null;
             next = null;
         }
@@ -57,15 +76,71 @@ public class Quiz implements Serializable
         //Copies i's data to j
         void questDupe(Question i, Question j)
         {
-            j.quesType = i.quesType;
-            j.quizQuestion = i.quizQuestion;
-            j.corrAns = i.corrAns;
-            j.wrongAns1 = i.wrongAns1;
-            j.wrongAns2 = i.wrongAns2;
-            j.wrongAns3 = i.wrongAns3;
-            j.truFal = i.truFal;
+            j.setQuesType(i.getQuesType());
+            j.setQuizQuestion(i.getQuizQuestion());
+            j.setCorrAns(i.getCorrAns());
+            j.setWrongAns1(i.getWrongAns1());
+            j.setWrongAns2(i.getWrongAns2());
+            j.setWrongAns3(i.getWrongAns3());
+            j.setTruFal(i.getTruFal());
         }
 
+
+        public String getQuizQuestion() {
+            return quizQuestion;
+        }
+
+        public void setQuizQuestion(String quizQuestion) {
+            this.quizQuestion = quizQuestion;
+        }
+
+        public int getQuesType() {
+            return quesType;
+        }
+
+        public void setQuesType(int quesType) {
+            this.quesType = quesType;
+        }
+
+        public String getCorrAns() {
+            return corrAns;
+        }
+
+        public void setCorrAns(String corrAns) {
+            this.corrAns = corrAns;
+        }
+
+        public String getWrongAns1() {
+            return wrongAns1;
+        }
+
+        public void setWrongAns1(String wrongAns1) {
+            this.wrongAns1 = wrongAns1;
+        }
+
+        public String getWrongAns2() {
+            return wrongAns2;
+        }
+
+        public void setWrongAns2(String wrongAns2) {
+            this.wrongAns2 = wrongAns2;
+        }
+
+        public String getWrongAns3() {
+            return wrongAns3;
+        }
+
+        public void setWrongAns3(String wrongAns3) {
+            this.wrongAns3 = wrongAns3;
+        }
+
+        public Boolean getTruFal() {
+            return truFal;
+        }
+
+        public void setTruFal(Boolean truFal) {
+            this.truFal = truFal;
+        }
     }
 
 
